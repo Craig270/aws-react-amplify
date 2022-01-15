2ff1ee93d8824f32daa07edbe6c4ae0ae7d92eb3
Craig Neff <ifly12610@gmail.com>
2022-01-15 00:50:31 -0700

    new website look

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -34,8 +34,18 @@ export default function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi there I'm </h2>
-          <h1>Craig Neff!</h1>
+          <h2>Hi there,</h2>
+          <h1>I'm Craig Neff!</h1>
+          <p>
+            {" "}
+            a Developer, Learner and Father living in Denver, Colorado.
+            Currently I am learning front end development working with React and
+            AWS. Currently I am a full time stay at home Dad for my two
+            wonderful boys and amazing Wife. In my free time I enjoy working on
+            personal React projects or practicing my JavaScript coding on the
+            website{" "}
+            <a href="https://www.codewars.com/users/Craig270">Code Wars.com!</a>
+          </p>
           <h3>
             Striving to be a better <span ref={textRef}></span>
           </h3>
